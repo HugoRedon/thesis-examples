6867235d2edd64d6a32d28acb05f69747de43e58
HugoRedon <hugo_redon_@hotmail.com>
2014-07-21 03:44:26 -0500

    dioxido de carbono metanol

---
+++ src/main/java/compounds/CompoundReader.java
@@ -0,0 +1,206 @@
+package compounds;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import termo.component.Compound;
+import termo.equations.Eqn101VaporPressure;
+import termo.equations.Eqn10VaporPressure;
+
+
+/**
+ *
+ * @author Hugo Redon Rivera
+ */
+public class CompoundReader {
+    private List<Compound> components = new ArrayList();
+    
+    
+    public Compound getCompoundByExactName(String name){
+    	if(components.size()==0){
+    		read();
+    	}
+		for(Compound compound: components){
+			if(compound.getName().equals(name.toLowerCase())){
+				return compound;
+			}
+		}
+    	return null;
+    }
+
+     public void read(){
+        try {
+            components.clear();
+            InputStream file = this.getClass().getResourceAsStream("/data/chemsep1.xml");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document document = db.parse(file);
+            
+           NodeList list =  document.getElementsByTagName("compound");
+           
+           for(int i  = 0; i < list.getLength() ; i++){
+               Node item = list.item(i);
+               
+                components.add(component(item));
+               
+           }
+            System.out.println("compounds found: " +list.getLength());
+            System.out.println("document" + document.getXmlVersion());
+            
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(CompoundReader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SAXException ex) {
+            Logger.getLogger(CompoundReader.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(CompoundReader.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
+  
+    
+    public String stringProperty(String propertyName, Element el){
+        Element elementValue =(Element) el.getElementsByTagName(propertyName).item(0);
+        
+        String value = elementValue.getAttribute("value");
+        return value;
+    }
+    public double numberProperty(String propertyName, Element el){
+        
+        
+        String value = stringProperty(propertyName, el);
+        System.out.println("value: " + value);
+        return Double.valueOf(value);
+    }
+    
+    public Compound component(Node node){
+        Element el = (Element)node;
+        String name = stringProperty("CompoundID", el);
+        System.out.println("name: " + name);
+        double ct = numberProperty("CriticalTemperature",el);
+        double cp = numberProperty("CriticalPressure",el);
+        double cv = numberProperty("CriticalVolume", el);
+        
+        double w = numberProperty("AcentricityFactor", el);
+        
+        
+        
+        Compound com = new Compound(name);
+        com.setCriticalTemperature(ct);
+        com.setCriticalPressure(cp);
+        com.setCriticalVolume(cv);
+        com.setAcentricFactor(w);
+        
+        if(hasEquationNumber("10", el)){
+            Eqn10VaporPressure eqn10 = getEquation10(el);
+            eqn10.setComponent(com);
+            com.setEqn10VaporPressure(eqn10);
+        }if(hasEquationNumber("101", el)){
+            Eqn101VaporPressure eqn101 = getEquation101(el);
+            eqn101.setComponent(com);
+            com.setEqn101VaporPressure(eqn101);
+        }
+        
+        
+//        fillVaporPressureList(com);
+        return com;
+        
+    }
+   
+    
+    
+    public boolean hasEquationNumber(String number, Element element){
+        Node vaporPressureNode = element.getElementsByTagName("VaporPressure").item(0);
+        
+        Element el = (Element )vaporPressureNode;
+        String num = stringProperty("eqno", el);
+        
+        return number.equals(num);
+    }
+    
+    
+    public Eqn10VaporPressure getEquation10(Element element){
+        Node vaporPressureNode10 = element.getElementsByTagName("VaporPressure").item(0);
+        
+        Element el = (Element )vaporPressureNode10;
+        Eqn10VaporPressure eqn = new Eqn10VaporPressure();
+        
+        double A = numberProperty("A", el);
+        double B = numberProperty("B", el);
+        double C = numberProperty("C", el);
+        
+        double tmin = numberProperty("Tmin", el);
+        double tmax = numberProperty("Tmax", el);
+        
+        eqn.setMinTemperature(tmin);
+        eqn.setMaxTemperature(tmax);
+        
+        eqn.setA(A);
+        eqn.setB(B);
+        eqn.setC(C);
+        
+        return eqn;
+    }
+    
+    public Eqn101VaporPressure getEquation101(Element element){
+        Node vaporPressureNode101 = element.getElementsByTagName("VaporPressure").item(0);
+        
+        Element el = (Element )vaporPressureNode101;
+        Eqn101VaporPressure eqn = new Eqn101VaporPressure();
+        
+        double A = numberProperty("A", el);
+        double B = numberProperty("B", el);
+        double C = numberProperty("C", el);
+        double D = numberProperty("D", el);
+        double E = numberProperty("E", el);
+        
+        double tmin = numberProperty("Tmin", el);
+        double tmax = numberProperty("Tmax", el);
+        
+        eqn.setMinTemperature(tmin);
+        eqn.setMaxTemperature(tmax);
+        
+        eqn.setA(A);
+        eqn.setB(B);
+        eqn.setC(C);
+        eqn.setD(D);
+        eqn.setE(E);
+        
+        return eqn;
+        
+    }
+
+    /**
+     * @return the components
+     */
+    public List<Compound> getComponents() {
+        if(components.isEmpty()){
+            read();
+        }   
+        return components;
+    }
+
+    /**
+     * @param components the components to set
+     */
+    public void setComponents(List<Compound> components) {
+        this.components = components;
+    }
+    
+   
+}
--- src/main/java/thesis/BinaryOptimizationFileGenerator.java
+++ src/main/java/thesis/BinaryOptimizationFileGenerator.java
@@ -12,6 +12,10 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import compounds.CompoundReader;
+import termo.activityModel.ActivityModel;
+import termo.activityModel.NRTLActivityModel;
+import termo.binaryParameter.ActivityModelBinaryParameter;
 import termo.binaryParameter.InteractionParameter;
 import termo.component.Compound;
 import termo.data.ExperimentalData;
@@ -24,13 +28,58 @@ import termo.eos.alpha.Alpha;
 import termo.eos.alpha.AlphaFactory;
 import termo.eos.mixingRule.MixingRule;
 import termo.eos.mixingRule.VDWMixingRule;
+import termo.eos.mixingRule.WongSandlerMixingRule;
 import termo.matter.HeterogeneousMixture;
 import termo.matter.HeterogeneousSubstance;
+import termo.optimization.ErrorData;
 import termo.optimization.errorfunctions.TemperatureMixtureErrorData;
 
 public class BinaryOptimizationFileGenerator extends FileGenerator{
-	public ExperimentalDataBinaryList getBinaryExperimentalListFromFile() throws IOException{
-		Path path = Paths.get("/home/hugo/Documents/repositories/MateriaLatex/data/binary.dat");
+	
+	public void generateFiles(String folderName) throws IOException{
+		//methanolWaterFiles(folderName+ "methanolWater/");
+		carbonDioxideMethanolFiles(folderName+"co2Met/");
+	}
+	
+	public ExperimentalDataBinaryList getBinaryExperimentalListFromFilePxy(String filePath) throws IOException{
+		Path path = Paths.get(filePath);
+		List<String> lines =Files.readAllLines(path);
+		
+		ExperimentalDataBinaryList blist = new ExperimentalDataBinaryList();
+		blist.setName("Ejemplo");
+		
+		
+		double temperature =273.15+ Double.valueOf(lines.get(0).split("\\s+")[1]);
+
+		List<ExperimentalDataBinary> list = new ArrayList();
+		
+		for(String line: lines){
+			 String[] lineWords = line.split("\\s+");
+			 
+			 System.out.println(line);
+			 
+			 try{
+				 double pressure =101325*Double.valueOf( lineWords[0]);
+				 double x =Double.valueOf( lineWords[1]);
+				 double y=Double.valueOf( lineWords[2]);
+				 
+				 ExperimentalDataBinary data = new ExperimentalDataBinary(temperature, pressure, x, y);
+				 list.add(data);
+			 }catch(Exception ex){
+				 System.out.println("Linea sin datos");
+			 }
+			 
+		}
+		blist.setReferenceComponent(getMethanol());
+		blist.setNonReferenceComponent(getWater());
+		blist.setList(list);
+		return blist;
+	}
+	
+	
+	
+	public ExperimentalDataBinaryList getBinaryExperimentalListFromFileTxy(String filePath) throws IOException{
+		Path path = Paths.get(filePath);
 		List<String> lines =Files.readAllLines(path);
 		
 		ExperimentalDataBinaryList blist = new ExperimentalDataBinaryList();
@@ -74,13 +123,63 @@ public class BinaryOptimizationFileGenerator extends FileGenerator{
 		
 
 	}
-	
-	
-	
-	public void generateFiles(String folderName) throws IOException{
+	public void carbonDioxideMethanolFiles(String folderName) throws IOException{
+		prepareFolder(folderName);
+		ExperimentalDataBinaryList blist = getBinaryExperimentalListFromFilePxy("/home/hugo/Documents/repositories/MateriaLatex/data/co2Met25.dat");
+		Cubic eos= EquationOfStateFactory.pengRobinsonBase();
+		Alpha alpha = AlphaFactory.getPengAndRobinsonExpression();
+		ActivityModel activityModel = new NRTLActivityModel();
+		MixingRule mr = new WongSandlerMixingRule(activityModel, eos);
+		
+		
+		CompoundReader reader = new CompoundReader();
+		
+		Compound cargonDioxide =reader.getCompoundByExactName("Carbon dioxide");
+		Compound methanol = reader.getCompoundByExactName("Methanol");
+		
+		
+		Set<Compound> compounds = new HashSet();
+		compounds.add(cargonDioxide);
+		compounds.add(methanol);
+		
+		ActivityModelBinaryParameter k = new ActivityModelBinaryParameter();
+		
+		HeterogeneousMixture mix = new HeterogeneousMixture(eos, alpha, mr, compounds, k);
+		
+		PrintWriter writer = new PrintWriter(folderName + "co2Met25.dat","UTF-8");
+		writer.println(" pressure x y calcPressure ycalc");
+		
+		double temperature = blist.getList().get(0).getTemperature();
+		mix.setTemperature(temperature);
+		
+//		0.5013 
+//		0.1187 
+//		0.3972 
+		//activityModel.setParameter(0.5013, methanol, cargonDioxide, k, index);
+		
+		k.getK().setSymmetric(true);
+		k.getK().setValue(cargonDioxide, methanol, 0.3972 );
+		
+		for (ExperimentalDataBinary data: blist.getList()){
+			double x = data.getLiquidFraction();
+			double y = data.getVaporFraction();
+			double pressure = data.getPressure();
+			
+			mix.setZFraction(cargonDioxide, x);
+			mix.setZFraction(methanol, 1-x);
+			mix.bubblePressure();
+			double calcpressure = mix.getPressure();
+			double ycalc=mix.getVapor().getReadOnlyFractions().get(cargonDioxide); 
+			
+			writer.println(" " + pressure  + " "+ x + " " + y+ " " + calcpressure + " " +  ycalc);
+		}
+		writer.close();
+		
+	}
+	public void methanolWaterFiles(String folderName) throws IOException{
 		prepareFolder(folderName);
 		
-		ExperimentalDataBinaryList blist = getBinaryExperimentalListFromFile();
+		ExperimentalDataBinaryList blist = getBinaryExperimentalListFromFileTxy("/home/hugo/Documents/repositories/MateriaLatex/data/binary.dat");
 		List<ExperimentalDataBinary> list = blist.getList(); 
 		
 		Cubic eos = EquationOfStateFactory.pengRobinsonBase();
@@ -104,10 +203,52 @@ public class BinaryOptimizationFileGenerator extends FileGenerator{
 	
 		generateBinaryTemperatureDiagramDataInFile(mix,folderName, "error.dat");
 		
+		generateAlphaOptim(mix,water,folderName,"errorWaterAlpha");
+		generateAlphaOptim(mix,methanol,folderName,"errorMethanolAlpha");
+		
+		System.out.println("stryjekMethanol k: "+ methanol.getK_StryjekAndVera());
+		System.out.println("stryjekWater k: "+ water.getK_StryjekAndVera());
+		
+		
+		mix.getErrorfunction().getOptimizer().setApplyErrorDecreaseTechnique(true);
 		mix.getErrorfunction().minimize();
 		generateBinaryTemperatureDiagramDataInFile(mix,folderName, "errorAfterOptim.dat");
 		
+	}
+	
+	
+
+
+	
+	
+	
+	public void generateAlphaOptim(HeterogeneousMixture mix,Compound compound, String folderPath,String fileNameWithouthExtension) throws FileNotFoundException, UnsupportedEncodingException{
+		Cubic eos = mix.getEquationOfState();
+		Alpha alpha = mix.getAlpha();
 		
+			
+		HeterogeneousSubstance substance = new HeterogeneousSubstance(eos,alpha,compound);
+		substance.getErrorFunction().setExperimental(compound.getExperimentalLists().iterator().next().getList());
+		substance.getErrorFunction().getOptimizer().setApplyErrorDecreaseTechnique(true);
+		
+		printAlphaOptimization(substance, folderPath, fileNameWithouthExtension + ".dat");
+		substance.getErrorFunction().minimize();
+		printAlphaOptimization(substance, folderPath, fileNameWithouthExtension + "AfterOptim.dat");
+		
+		
+	}
+	
+	
+	public void printAlphaOptimization(HeterogeneousSubstance substance,String folderPath,String fileName) throws FileNotFoundException, UnsupportedEncodingException{
+		PrintWriter writer = new PrintWriter(folderPath + fileName,"UTF-8");
+		writer.println(" temperature expPressure calcPressure");
+		for(ErrorData error: substance.getErrorFunction().getErrorForEachExperimentalData()){
+			double temperature = error.getTemperature();
+			double expPressure = error.getExperimentalPressure();
+			double calcPressure =error.getCalculatedPressure();
+			writer.println(" "+temperature + " "+ expPressure + " "+calcPressure );
+		}
+		writer.close();
 	}
 	
 	
